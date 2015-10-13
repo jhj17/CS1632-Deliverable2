14304c6a69b8e463a3300261ebbe8c85d958465d
jhj17 <jeff_josephs15@yahoo.com>
2015-10-13 13:29:16 -0400

    Added comments to test functions

--- test/TestCitySim9000.java
+++ test/TestCitySim9000.java
@@ -5,6 +5,7 @@ public class TestCitySim9000 {
 
 	@Test
 	// Test parse arguments with a character
+	// Test should result in an invalid input
 	public void testParseArgumentsChar() {
 		String[] testArgs = new String[]{"a"};
 		int target = -1;        
@@ -14,6 +15,7 @@ public class TestCitySim9000 {
 
 	@Test
 	// Test parse arguments with a positive integer
+	// Test should result in a valid input
 	public void testParseArgumentsSuccess() {
 		String[] testArgs = new String[]{"7"};
 		int target = 7;        
@@ -23,6 +25,7 @@ public class TestCitySim9000 {
 
 	@Test
 	// Test parse arguments with a negative integer
+	// Test should result in an invalid input
 	public void testParseArgumentsNeg() {
 		String[] testArgs = new String[]{"-10"};
 		int target = -1;        
@@ -32,6 +35,7 @@ public class TestCitySim9000 {
 
 	@Test
 	// Test parse arguments with multiple arguments
+	// Test should result in an invalid input
 	public void testParseArgumentsMultiple() {
 		String[] testArgs = new String[]{"a","1"};
 		int target = -1;        
